a6492035f5973e9c99ee7f7cbaf7b9cce5596f5c
ch98-1 <akerunakayama+1@gmail.com>
2014-10-30 22:30:56 -0700

    added time functions.

--- c/Random.c
+++ c/Random.c
@@ -20,6 +20,8 @@ void File(const unsigned long int size){//run test for file. Takes in size in ki
 	unsigned long int i, j, k;//counters i, j and k
 	unsigned long long int zeros = 0;
 	unsigned long long int ones = 0;//number of zeros and ones
+	unsigned long long int total = 0;//total bits
+	time_t start = time(NULL);
 	for (i = 0; i < 1024; i++){//1 kb
 		printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");//clear
 		printf("%.1f%% finished", (float)(100 * i) / 1024);//display percentage finished
@@ -32,24 +34,27 @@ void File(const unsigned long int size){//run test for file. Takes in size in ki
 				else{//if it was zero
 					zeros++;//add one to zeros
 				}
+				total++;//increment total
 			}
 		}
 	}
 	printf("\n\nChecked %uKB ", size);//size of file
-	printf("(%u Bits)\n", (ones + zeros));//size in bits
+	printf("(%u Bits)\n", total);//size in bits
 	printf("\nResult:\n\nnumber of bits:\n%u zeros\n", zeros);//display result. number of zeros
 	printf("%u ones\n", ones);//number of ones
-	printf("%.5f%% binary difference\n", (float)abs(ones - zeros) * 100 / (ones + zeros));//zero and one diference
+	printf("%.5f%% binary difference\n\n", (float)abs(ones - zeros) * 100 / (total));//zero and one diference
+	printf("%.1f seconds\n", difftime(time(NULL), start));//display time taken at end
 }
 
 void CRand(const unsigned long int size){//run test for c random function. takes in size in kilobytes.
-	srand((int)time(NULL) + (int)clock());
 	unsigned long int i, j, k;//counters i, j and k
 	unsigned long long int zeros = 0;
 	unsigned long long int ones = 0;//number of zeros and ones
+	unsigned long long int total = 0;//total bits
+	time_t start = time(NULL);
 	for (i = 0; i < 1024; i++){//1 kb
 		printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");//clear
-		printf("%.1f%% finished", (float)(100* i)/1024);//display percentage finished
+		printf("%.1f%% finished", (float)(100 * i) / 1024);//display percentage finished
 		for (j = 0; j < size; j++){//number of kilobyte
 			unsigned char rand = GetCRandom();//get random char
 			for (k = 0; k < 8; k++){//for each bit in char
@@ -59,13 +64,15 @@ void CRand(const unsigned long int size){//run test for c random function. takes
 				else{//if it was zero
 					zeros++;//add one to zeros
 				}
+				total++;//increment total
 			}
 		}
 	}
 	printf("\n\nChecked %uKB ", size);//size of file
-	printf("(%u Bits)\n", (ones + zeros));//size in bits
+	printf("(%u Bits)\n", total);//size in bits
 	printf("\nResult:\n\nnumber of bits:\n%u zeros\n", zeros);//display result. number of zeros
 	printf("%u ones\n", ones);//number of ones
-	printf("%.5f%% binary difference\n", (float)abs(ones - zeros) * 100 / (ones + zeros));//zero and one diference
+	printf("%.5f%% binary difference\n\n", (float)abs(ones - zeros) * 100 / (total));//zero and one diference
+	printf("%.1f seconds\n", difftime(time(NULL), start));//display time taken at end
 }
 
